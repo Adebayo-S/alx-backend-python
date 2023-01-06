11626038e781fa3e8e18a58ddfdee1c5de4207fa
Adebayo Samuel <eladebayoor@gmail.com>
2023-01-06 04:38:27 -0500

    [style]: added line space

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """ Let's duck type an iterable object"""
 from typing import Sequence, Iterable, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ Annotate function element_length """
     return [(i, len(i)) for i in lst]
