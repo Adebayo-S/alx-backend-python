6845e302e3e09cbbfb69fad01f444bfeca2aed94
Adebayo Samuel <eladebayoor@gmail.com>
2023-01-29 21:10:24 -0500

    [feat]: TestGetJson class created

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 """Parameterize a unit test
 """
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch
 from parameterized import parameterized
 
 
@@ -32,3 +33,18 @@ class TestAccessNestedMap(unittest.TestCase):
 
         self.assertEqual(
             f'KeyError({str(error.exception)})', repr(error.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """ Class to test get_json """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Mock HTTP calls
+        """
+        with patch('requests.get') as mock_request:
+            mock_request.return_value.json.return_value = test_payload
+            self.assertEqual(get_json(url=test_url), test_payload)
