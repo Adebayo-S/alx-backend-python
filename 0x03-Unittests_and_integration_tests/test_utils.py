3c52dee16b3597e28d2f44b258d45aec600f9e63
Adebayo Samuel <eladebayoor@gmail.com>
2023-01-29 20:53:43 -0500

    [feat]: parametixe unit test

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Parameterize a unit test
+"""
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test access_nested_map utils methods
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ test that the method returns what it is supposed to.
+         """
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
